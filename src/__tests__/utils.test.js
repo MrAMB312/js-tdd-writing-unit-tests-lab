a2b2c56ed36447afaa62567310efaac7e3ae8996
Alex Blomarz <a.blomarz@gmail.com>
2025-04-12 20:56:24 -0500

    complete js-tdd-writing-unit-tests-lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,33 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true when the word is the same forward and backward", () => {
+        const word = "racecar";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(true);
+    });
+    it("returns false when the word is not the same forward and backward", () => {
+        const word = "car";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(false);
+    });
+    it("returns true for words that are a combination of uppercase and lowercase letters", () => {
+        const word = "RAceCar";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(true);
+    });
+    it("returns false for an empty string", () => {
+        const word = "";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(false);
+    })
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+function reverse(string) {
+    return string.split("").reverse().join("");
+}
+
+export function isPalindrome(word) {
+    if (word === "") {
+        return false;
+    } else {
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+    };
+}
